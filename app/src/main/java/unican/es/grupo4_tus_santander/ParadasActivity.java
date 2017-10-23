7d37ba3558d9ecc73959af37450221d8127d6456
Guillermo <guillermo_argumosa@hotmail.es>
2017-10-23 11:34:02 +0200

    Añadidas clases Linea, Parada  y ParadasActivity

--- app/src/main/java/unican/es/grupo4_tus_santander/ParadasActivity.java
+++ app/src/main/java/unican/es/grupo4_tus_santander/ParadasActivity.java
@@ -1,11 +1,20 @@
 package unican.es.grupo4_tus_santander;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
 
 /**
  * Created by guill on 23/10/2017.
  */
 
 public class ParadasActivity extends Activity {
-
+    ArrayList<Parada> paradas = new ArrayList<Parada>();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paradas);
+    }
 }

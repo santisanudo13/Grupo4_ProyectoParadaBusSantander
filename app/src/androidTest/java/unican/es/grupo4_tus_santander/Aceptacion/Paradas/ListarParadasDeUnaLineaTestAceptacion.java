caeeeb8fc3d498137e4184788d665391c2e0ca32
Asier Lopez Uriona <asier.lopezu@alumnos.unican.es>
2017-10-29 23:35:50 +0100

    Creado plan de pruebas (sin terminar) y estructura para los test.

---
+++ app/src/androidTest/java/unican/es/grupo4_tus_santander/Aceptacion/Paradas/ListarParadasDeUnaLineaTestAceptacion.java
@@ -0,0 +1,11 @@
+package unican.es.grupo4_tus_santander.Aceptacion.Paradas;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ListarParadasDeUnaLineaTestAceptacion {
+
+}
